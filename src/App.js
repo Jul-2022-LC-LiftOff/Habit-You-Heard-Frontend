13dfaf9e0d914f46638a78d1ee11397b19b2d751
salev <salevillain@gmail.com>
2022-08-05 10:08:27 -0500

    material UI installed, sign-in page component plugged in app for show.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material'
+import Button from '@mui/material/Button';
+import SignInSide from './components/SignInSide';
+
+const theme = createTheme({
+  palette: {
+    mode: "dark"
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SignInSide />
+    </ThemeProvider>
   );
 }
 
