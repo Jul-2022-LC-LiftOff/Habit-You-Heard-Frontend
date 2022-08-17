3d86cf079c0b1f97d5f2952d69928a988ae7f20f
erikkustrife <erikkustrife@gmail.com>
2022-08-16 20:44:22 -0500

    Calender first draft

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material'
 import Button from '@mui/material/Button';
 import SignInSide from './components/SignInSide';
-
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'src/Calendar.css';
 
 const theme = createTheme({
   palette: {
@@ -13,7 +15,19 @@ const theme = createTheme({
 })
 
 function App() {
-  return (
+const [date, setDate] = useState(new Date());
+//beginning of calender segment
+ <div className='app'>
+      <h1 className='text-center'>React Calendar</h1>
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
+    </div>
+    //end of calender segment
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <SignInSide />
