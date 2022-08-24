f0a75bdebb02b22cd17170158019ec3212f986a3
salev <salevillain@gmail.com>
2022-08-24 13:17:25 -0500

    dark theme somewhat implemented

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { Outlet, Link } from "react-router-dom";
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import {
+  Button,
+  createTheme,
+  Paper,
+  ThemeProvider,
+  Typography
+} from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline"
+import { useState } from 'react'
 
 const Layout = () => {
+  const [darkMode, setDarkMode] = useState(false)
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : 'light'
+    }
+  })
   return (
-
-
+  <ThemeProvider theme={theme}>
+    <CssBaseline>
       <Outlet />
+      <button id='dark-mode' onClick={() => {setDarkMode(!darkMode)}}>Toggle dark/light Mode</button>
+    </CssBaseline>
+  </ThemeProvider>
 
-  )
+)
 };
 
 export default Layout;
