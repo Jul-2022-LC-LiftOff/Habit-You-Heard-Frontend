04c596c8865266bcc1ee416eb48f9138ed5a08b3
salev <salevillain@gmail.com>
2022-09-19 20:57:52 -0500

    toggle drak mode cool button

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -2,17 +2,19 @@ import React, { useState } from "react";
 import IconButton from '@mui/material/IconButton';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { styled } from "@mui/material/styles";
 
 
-
+const StyledIcon = styled(IconButton)(({theme}) => ({
+    color: theme.palette.text.secondary
+  }));
 
 const ThemeToggle = (props) => {
     
     return (
-        <IconButton id='dark-mode' onClick={() => props.onToggleTheme()} style={{ fontSize: 15 }}>
-            {props.darkMode ? "dark mode" :  "light mode"}
+        <StyledIcon id='dark-mode' onClick={() => props.onToggleTheme()}>
             {props.darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
-        </IconButton>
+        </StyledIcon>
   )
   };
   
